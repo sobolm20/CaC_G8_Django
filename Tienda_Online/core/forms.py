1288b8eec8f35e5b0046337d81ad2d3249956c6c
Gisela Fredes <giserofredes79@gmail.com>
2023-10-05 18:47:59 -0300

    validaciones

--- Tienda_Online/core/forms.py
+++ Tienda_Online/core/forms.py
@@ -1,18 +1,25 @@
+from typing import Any
 from django import forms
 
 palabras_prohibidas=['forro','pito', 'laca']
+
 class ContactForm(forms.Form):
     nombre = forms.CharField(label="Nombre:", max_length=30, required=True)
     apellido = forms.CharField(label="Apellido:", max_length=30, required=True)
     email = forms.EmailField(label="E-mail", required=True)
     telefono = forms.IntegerField(label="Número de teléfono", required=False)
     consulta = forms.CharField(label="Ingrese su consulta:",required=False, widget=forms.Textarea)
-    def contiene_prohibidas(valor):
+    def contiene_prohibidas(self, valor):
          for palabra in palabras_prohibidas:
             if palabra.lower() in valor.lower():
                 raise forms.ValidationError('El campo contiene palabras groseras o inapropiadas.')
-    
+        
     def clean_nombre(self):
         valor = self.cleaned_data.get('nombre')
+        self.contiene_prohibidas(valor)
+        return valor
+    
+    def clean_apellido(self):
+        valor = self.cleaned_data.get('apellido')
         self.contiene_prohibidas(valor)
         return valor
