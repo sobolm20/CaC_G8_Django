a3303ae95693e8d58624ccb5a9b5e35002a40ad8
Lorena <zolsslorena@gmail.com>
2023-10-05 21:17:24 -0300

    Cambios en el forms.py

--- Tienda_Online/core/forms.py
+++ Tienda_Online/core/forms.py
@@ -1,14 +1,35 @@
 from typing import Any
 from django import forms
+from django.forms import ValidationError
 
-palabras_prohibidas=['forro','pito', 'laca']
+palabras_prohibidas=['forro','pito', 'loca']
 
 class ContactForm(forms.Form):
-    nombre = forms.CharField(label="Nombre:", max_length=30, required=True)
-    apellido = forms.CharField(label="Apellido:", max_length=30, required=True)
-    email = forms.EmailField(label="E-mail", required=True)
-    telefono = forms.IntegerField(label="Número de teléfono", required=False)
-    consulta = forms.CharField(label="Ingrese su consulta:",required=False, widget=forms.Textarea)
+    nombre = forms.CharField(
+        label="Nombre:", 
+        max_length=30, 
+        required=True, 
+        error_messages={'required': 'Por favor, complete el campo'},
+        widget=forms.TextInput(attrs={'placeholder':'Ingrese aquí su nombre'}))
+    apellido = forms.CharField(
+        label="Apellido:", 
+        max_length=30, 
+        required=True,
+        error_messages={'required': 'Por favor, complete el campo'},
+        widget=forms.TextInput(attrs={'placeholder':'Ingrese aquí su apellido'}))
+    email = forms.EmailField(
+        label="E-mail", 
+        required=True, 
+        error_messages={'required': 'Por favor, complete el campo'}, 
+        widget=forms.EmailInput(attrs={'type': 'email','placeholder':'xx@yy.zz'}))
+    telefono = forms.IntegerField(
+        label="Número de teléfono", 
+        required=False)
+    consulta = forms.CharField(
+        label="Ingrese su consulta:", 
+        required=False, 
+        widget=forms.Textarea(attrs={'rows': 7, 'placeholder':'Ingrese aquí su consulta'}))
+    
     def contiene_prohibidas(self, valor):
          for palabra in palabras_prohibidas:
             if palabra.lower() in valor.lower():
